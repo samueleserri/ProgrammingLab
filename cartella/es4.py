6130222c271c6bebea2b792cee02f3e7f0187777
samueleserri <115220983+samueleserri@users.noreply.github.com>
2022-11-27 13:37:49 +0000

    10/10 VERSION

--- cartella/es4.py
+++ cartella/es4.py
@@ -1,23 +1,23 @@
-class CSVfile():
+class CSVFile():
+    
     def __init__(self, name):
         self.name=name
 
-        def get_data(self):
-            values = []
-            
+    def get_data(self):
+        values = []
+        try:
             my_file=open(self.name, 'r')
-        
-            for line in my_file:
-                elements=line.split(',')
-                if elements[0]!='Date':
-                    value=elements[1]
-                    values.append(float(value))
-            my_file.close()
+        except:
+            print('Errore, inserire un file')
+        if my_file == []:
+            return None       
+        for line in my_file:
+            elements=line.strip('\n').split(',')
+            if elements[0]!='Date':
+                values.append(elements)   
+        my_file.close()
+        return values    
 
-            list = []
-            for item in values:
-                list.append(item)
-
-            return list
-
-    
+#csvfile=CSVfile('shampo_sales.csv')
+#data=csvfile.get_data()
+#print(data)
